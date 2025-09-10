8541d5b4c4a13868049b6806edf938cd2ca9ac83
Julyan Sapp <Julyan11d13sapp@gmail.com>
2025-09-10 16:34:30 +0000

    created a program that can calculate the remaining fuel in a vehicle

--- main.cpp
+++ main.cpp
@@ -2,14 +2,29 @@
 // TODO: YOUR NAME HERE
 #include <iostream>
 // TODO: Add the "using" statement
-
+using namespace std;
 int main() {
   // TODO: Declare your variables, with comments for each
+  // capacity of the fuel tank in gallons
+  double capacity;
+  //miles per gallon
+  double mpg;
+  //percentage filled
+  int percentFull;
+
 
   // TODO: Prompt for and input the required values
-
+  cout << "Please input the capacity of your fuel tank: ";
+  cin >> capacity;
+  cout << "Please input the mpg of your car: ";
+  cin >> mpg;
+  cout << "Please enter percent full: ";
+  cin >> percentFull;
   // TODO: Calculate the results
 
-  // TODO: Output the results
+  double remainingFuel = (percentFull*capacity)/100;
+  double milesLeft = remainingFuel*mpg;
   
+  // TODO: Output the results
+  cout <<" The vehicle can be driven " << milesLeft << " on the remaining fuel. " << endl;
 }
